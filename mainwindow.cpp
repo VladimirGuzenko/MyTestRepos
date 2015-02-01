50ce2fe705bc98d2bc4f864a333eedb385e1b483
VladimirGuzenko <niore82@gmail.com>
2015-02-01 22:32:13 +0400

    tets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,40 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+QString MainWindow::labelText() {
+    return ui->Label1->text();
+}
+
+void MainWindow::setLabelText(const QString &text) {
+    ui->Label1->setText(text);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QNetworkAccessManager *manager = new QNetworkAccessManager(this);
+    QObject::connect(manager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(replyFinished(QNetworkReply*)));
+
+    QNetworkRequest request;
+    request.setUrl(QUrl("http://niore.ru"));
+    request.setRawHeader("User-Agent", "MyOwnBrowser 1.0");
+
+    QNetworkReply *reply = manager->get(request);
+
+
+
+}
+
+ void MainWindow::replyFinished(QNetworkReply* object)
+{
+
+    MainWindow::setLabelText("first line");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
 
 namespace Ui {
 class MainWindow;
@@ -12,8 +14,15 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QString labelText();
+    void setLabelText(const QString &string);
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+signals:
+    void finished(QNetworkReply* object);
+private slots:
+    void replyFinished(QNetworkReply* object);
+    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
